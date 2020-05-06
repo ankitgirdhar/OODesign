ae97632ec267f68c1404f4999708be5f86331147
ankitgirdhar <stanleycooper136@gmail.com>
2020-05-06 10:42:11 +0530

    initializing splitwise

--- Elevator/src/Controller.java
+++ Elevator/src/Controller.java
@@ -1,5 +1,5 @@
 class Controller {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        
     }
 }
---
+++ splitwise_OOD/src/com/machine_coding/Main.java
@@ -0,0 +1,8 @@
+package com.machine_coding;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
